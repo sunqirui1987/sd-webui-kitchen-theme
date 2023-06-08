a248c37c47894242a6a02d273b2c862576ce0509
sunqirui1987 <suiqirui1987@qq.com>
2023-06-08 14:54:24 +0800

    Update style.tsx

--- src/components/Header/style.tsx
+++ src/components/Header/style.tsx
@@ -16,9 +16,9 @@ export const themeIcon = {
 };
 
 export const darkLogo =
-  'https://gw.alipayobjects.com/zos/bmw-prod/9ecb2822-1592-4cb0-a087-ce0097fef2ca.svg';
+  'https://svgsilh.com/svg/304967.svg';
 export const lightLogo =
-  'https://gw.alipayobjects.com/zos/bmw-prod/e146116d-c65a-4306-a3d2-bb8d05e1c49b.svg';
+  'https://svgsilh.com/svg/304967.svg';
 
 export const civitaiLogo = (
   <span className="anticon civitai" role="img">
